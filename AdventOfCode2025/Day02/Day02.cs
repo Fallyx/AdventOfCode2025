f62475af965948ca919c7d06cdb2e22b19a49f85
Fallyx <Fallyx@users.noreply.github.com>
2025-12-03 06:30:16 +0100

    Day 02

---
+++ AdventOfCode2025/Day02/Day02.cs
@@ -0,0 +1,46 @@
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2025.Day02;
+
+public class Day02
+{
+    const string inputPath = @"Day02/Input.txt";
+    private static long sumInvalidIdsPart1 = 0;
+    private static long sumInvalidIdsPart2 = 0;
+
+    internal static void Task1()
+    {
+        List<string> productIdRanges = [.. File.ReadLines(inputPath).First().Split(',')];
+
+        foreach (string range in productIdRanges)
+        {
+            string[] bounds = range.Split('-');
+            long lower = long.Parse(bounds[0]);
+            long upper = long.Parse(bounds[1]);
+
+            for(long i = lower; i<= upper; i++)
+            {
+                string num = i.ToString();
+                string left = num[.. (num.Length / 2)];
+                string right = num[(num.Length / 2) ..];
+
+                if (left == right)
+                    sumInvalidIdsPart1 += i;
+
+                for (int x = 1; x <= num.Length / 2; x++)
+                {
+                    List<string> nums = [.. Regex.Matches(num, ".{1," + x + "}").Cast<Match>().Select(m => m.Value)];
+
+                    if (nums.All(n => n == nums[0]))
+                    {
+                        sumInvalidIdsPart2 += i;
+                        break;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine($"Task 1: {sumInvalidIdsPart1}");
+        Console.WriteLine($"Task 2: {sumInvalidIdsPart2}");
+    }
+}
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -35,6 +35,9 @@ internal class Program
             case 1:
                 Day01.Day01.Task1and2();
                 break;
+            case 2:
+                Day02.Day02.Task1();
+                break;
         }
 
         stopwatch.Stop();
